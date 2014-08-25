c6e40d1c49f3dc02b4670d61f19adf91056c35d4
Peter Williams <pwilliams@square-9.com>
2014-08-25 17:34:22 -0400

    Initial Commit

---
+++ Square9Analytics/Controllers/ActionsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Square9Analytics.Controllers
+{
+    /// <summary>
+    /// API Endpoint for action-based analytics data requests.
+    /// </summary>
+    public class ActionsController : AnalyticsController
+    {
+        // GET api/actions
+        public HttpResponseMessage GetSomething()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, "Hello World");
+        }
+
+        // POST api/actions
+        public HttpResponseMessage Post([FromBody]string value)
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, "Posted");
+        }
+
+        // DELETE api/actions/5
+        public HttpResponseMessage Delete(int id)
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, "Deleted");
+        }
+    }
+}
