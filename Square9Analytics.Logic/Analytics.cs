02b929b5831282c1e6af44287d6fa24d8445930e
rkinrade <rkinrade@square-9.com>
2014-08-27 15:55:41 -0400

    updates and merging

--- Square9Analytics.Logic/Analytics.cs
+++ Square9Analytics.Logic/Analytics.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Square9Analytics.Objects;
 
 namespace Square9Analytics.Logic
 {
     public class Analytics
     {
+        public Int32 getActionCount(DateTime startDate, DateTime endDate, AuditEntry action)
+        {
+            Int32 iReturn = 0;
+
+            if (startDate < endDate)
+            {
+                DataAccess.DataAnalytics da = new DataAccess.DataAnalytics();
+
+                Int32 iCount = da.getActionCount(startDate, endDate, action);
+
+                TimeSpan diff = endDate.Subtract(startDate);
+
+                if (iCount >= diff.Days)
+                {
+                    iReturn = iCount / diff.Days;
+                }
+
+            }
+
+            return iReturn;
+        }
+
+       
     }
 }
