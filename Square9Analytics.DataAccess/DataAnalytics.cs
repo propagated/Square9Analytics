a401e5251491dcd546e9ee6acdec1f857d4c948a
Nicholas <ntrue@square-9.com>
2014-08-27 09:08:39 -0400

    Added getActionCount Function, which takes three inputs startDate, endDate, and an action and returns the number of rows in SQL meeting that criteria

--- Square9Analytics.DataAccess/DataAnalytics.cs
+++ Square9Analytics.DataAccess/DataAnalytics.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using Square9Analytics.Objects;
 
 namespace Square9Analytics.DataAccess
 {
     public class DataAnalytics
     {
+        public int getActionCount(DateTime startDate, DateTime endDate, String action)
+        {
+            int actionCount = 0;
+            using (SqlConnection sqlConnection = new SqlConnection("Data Source=(local)\\GETSMART;Initial Catalog=SmartSearch;Integrated Security=SSPI;MultipleActiveResultSets=true"))
+            {
+                
+                sqlConnection.Open();
+                string sql = "SELECT Count(*) AS Count FROM ssAudit WHERE ACTION LIKE '%" + action + "%' AND Date BETWEEN'" + startDate.ToString("yyyy-MM-dd HH:mm:ss") + "' AND '" + endDate.ToString("yyyy-MM-dd HH:mm:ss") + " 23:59:59.999'";
+                using (var command = new SqlCommand(sql, sqlConnection))
+                {
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            actionCount = Convert.ToInt16(reader["Count"]);
+                        }
+                    }
+                }
+                sqlConnection.Close();
+
+            }
+           return actionCount; 
+        }
     }
 }
