0e89aa24a49a7f9146325ce02d010ad635c3504a
propagated <pwilliams@square-9.com>
2014-09-09 11:45:39 -0400

    DA adding time twice to enddate

--- Square9Analytics.DataAccess/DataAnalytics.cs
+++ Square9Analytics.DataAccess/DataAnalytics.cs
@@ -14,18 +14,17 @@ namespace Square9Analytics.DataAccess
         public int getActionCount(DateTime startDate, DateTime endDate, AuditEntry action)
         {
             int actionCount = 0;
-            using (SqlConnection sqlConnection = new SqlConnection("Data Source=(local)\\GETSMART;Initial Catalog=SmartSearch;Integrated Security=SSPI;MultipleActiveResultSets=true"))
+            using (var sqlConnection = new SqlConnection("Data Source=(local)\\GETSMART;Initial Catalog=SmartSearch;Integrated Security=SSPI;MultipleActiveResultSets=true"))
             {
-                
                 sqlConnection.Open();
-                string sql = "SELECT Count(*) AS Count FROM ssAudit WHERE ACTION LIKE '%" + action + "%' AND Date BETWEEN'" + startDate.ToString("yyyy-MM-dd HH:mm:ss") + "' AND '" + endDate.ToString("yyyy-MM-dd HH:mm:ss") + " 23:59:59.999'";
+                string sql = "SELECT Count(*) AS Count FROM ssAudit WHERE ACTION LIKE '%" + action + "%' AND Date BETWEEN'" + startDate.ToString("yyyy-MM-dd HH:mm:ss") + "' AND '" + endDate.ToString("yyyy-MM-dd") + " 23:59:59.999'";
                 using (var command = new SqlCommand(sql, sqlConnection))
                 {
-                    using (var reader = command.ExecuteReader())
+                    using (SqlDataReader reader = command.ExecuteReader())
                     {
                         while (reader.Read())
                         {
-                            actionCount = Convert.ToInt16(reader["Count"]);
+                            actionCount = Convert.ToInt32(reader["Count"]);
                         }
                     }
                 }
