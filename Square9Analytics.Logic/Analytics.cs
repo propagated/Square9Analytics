f0f944a047a52c2d638df7c1f460902de1c31152
jonhall24 <jhall@square-9.com>
2014-09-30 19:31:21 -0400

    Adding in new function and new object
    This is a new function that handles the parsing of the object then will
    return a list of this object (still needs work, but is being looked at)

--- Square9Analytics.Logic/Analytics.cs
+++ Square9Analytics.Logic/Analytics.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data;
 using System.Threading.Tasks;
 using Square9Analytics.Objects;
 
@@ -33,16 +34,37 @@ namespace Square9Analytics.Logic
                     // New exception added in here for the next layer of checking - Jon H. 9/12
                     // Once the exception has been hit, going to set iReturn to 0 and break out of function
                     throw new Exception("The difference in days is less than 0");
-                    return iReturn;
                 }
             }
             else
             {
                 
                 throw new Exception("Current Date Range is invalid, End Date needs to be greater than Start Date");
-                return iReturn;
             }
             return iReturn;
-        }     
+        }    
+
+        // New function to be able to return an object so that API can parse through it and display data
+        public List<DataObject> getActionTable(DateTime startDate, DateTime endDate, AuditEntry action)
+        {
+            // Create a list to be able to store the data
+            List<DataObject> objectList = new List<DataObject>();
+            DataAccess.DataAnalytics da = new DataAccess.DataAnalytics();
+
+            // Store the data gotten from DA and store it in a Temp variable
+            DataTable dataTable = new DataTable();
+
+            // This is just something temporary while I wait to get the function name that DA is using (if it's different)
+            dataTable = da.getActionTable(startDate, endDate, action);
+
+            // Parse through the temp variable and store it into the list object
+            foreach( DataRow dr in dataTable.Rows )
+            {
+                objectList.Add(new DataObject() { ID = (int)dr["ID"], Date = (DateTime)dr["Date Entered"], UserName = dr["UserName"].ToString() });
+            }
+            
+            // Once the DataTable is parsed through, be able to return the list object up
+            return objectList;
+        }
     }
 }
---
+++ Square9Analytics.Objects/DataObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Square9Analytics.Objects
+{
+    // To be able to store the data from a table
+    public class DataObject
+    {
+        public int ID { get; set; }
+        public DateTime Date { get; set; }
+        public string UserName { get; set; }
+    }
+}
