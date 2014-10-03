8f7deb904c40bab5cf212c134997c4507fb184a2
JHall <jhall@square-9.com>
2014-10-03 19:04:18 -0400

    adding it back in

---
+++ Square9Analytics.Objects/DataObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Square9Analytics.Objects
+{
+    // The object that will store the data from DA
+    public class DataObject
+    {
+        public int ID { get; set; }
+        public DateTime Date { get; set; }
+        public string UserName { get; set; }
+    }
+}
