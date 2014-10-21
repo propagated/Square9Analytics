39deb34b804505e38883081beaee43beebe85725
Bobby Byrnes <rbyrnes@square-9.com>
2014-10-21 18:33:30 -0400

    Now returns JSON!
    Now returns what it should.

--- Square9Analytics.Logic/Analytics.cs
+++ Square9Analytics.Logic/Analytics.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data;
 using System.Threading.Tasks;
 using Square9Analytics.Objects;
 
@@ -41,7 +42,56 @@ namespace Square9Analytics.Logic
 
                 throw new Exception("Current Date Range is invalid, End Date needs to be greater than Start Date");
             }
+
             return fReturn;
-        }     
+        }
+
+        public AuditLog getAuditLog(DateTime startDate, DateTime endDate, AuditAction auditAction, string UserName = "")
+        {
+            AuditLog auditLog = new AuditLog();
+            List<string> users = new List<string>();
+            string auditUser = UserName;
+
+            // Create the DataTable, and then clear the DataTable so that it is empty.
+            DataTable auditTable = new DataTable();
+            auditTable.Clear();
+
+            DataAccess.DataAnalytics da = new DataAccess.DataAnalytics();
+
+            // Fill in the datable with what is returned from DataAccess
+            //auditTable = da.getActions(startDate, endDate, userName);
+
+            // This would parse through the table given from DA and then return it up to the API layer
+            foreach (DataRow dr in auditTable.Rows)
+            {
+                users.Add(dr["Users"].ToString());
+                auditLog.Log.Add(new AuditEntry() { Date = (DateTime)dr["Date"], Action = (AuditAction)dr["Action"] });
+            }
+
+            auditLog.Users = users.Distinct().ToList();
+
+            /*
+            if (auditUser.Length > 0)
+            {
+                foreach (DataRow d in auditTable.Rows)
+                {
+                    if (auditUser == d["Users"].ToString())
+                    {
+                        auditLog.Users.Add(d["Users"].ToString());
+                    }
+                }
+
+                foreach (DataRow drow in auditTable.Rows)
+                {
+                    if (auditUser == drow["Users"].ToString())
+                    {
+                        auditLog.Log.Add(new AuditEntry() { Date = (DateTime)drow["Date"], Action = (AuditAction)drow["Action"] });
+                    }
+                }
+            }
+            */
+
+            return auditLog;
+        }
     }
 }
