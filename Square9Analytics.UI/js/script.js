21ad8f452acfea052e3af864b3c809022d7866cb
pwilliams <pwilliams@square-9.com>
2015-01-12 17:09:58 -0500

    fix indenting

--- Square9Analytics.UI/js/script.js
+++ Square9Analytics.UI/js/script.js
@@ -10,55 +10,55 @@ $(function() {
     //init chart
     chart = c3.generate({
     	data: {
-            x: 'x',
-            columns: []
-        },
-        axis: { 
-          y: { 
-              label: { 
-                  text: 'Number of actions taken.', position: 'outer-middle'
-              }
-          },
-          x: {
-            label: {
-                text: 'Action Dates',
-                position: 'inner-left'
-            },
-            type : 'timeseries',
-            tick: {
-              fit: true,
-              format: "%m-%d-%Y",
-              rotate:45,
+    		x: 'x',
+    		columns: []
+    	},
+    	axis: { 
+    		y: { 
+    			label: { 
+    				text: 'Number of actions taken.', position: 'outer-middle'
+    			}
+    		},
+    		x: {
+    			label: {
+    				text: 'Action Dates',
+    				position: 'inner-left'
+    			},
+    			type : 'timeseries',
+    			tick: {
+    				fit: true,
+    				format: "%m-%d-%Y",
+    				rotate:45,
 
           		//culling: false, //show all ticks (dates may overlap with big data sets)
-              culling: {
+          		culling: {
                 max: 15 // the number of tick texts will be adjusted to less than this value
               }
             }
           }
         },
         padding:{
-            right:50,
-            left:50
+        	right:50,
+        	left:50
         }
-    });
+      });
     
     //init date range picker
     startDate = moment().subtract(2, 'months').format("MM/DD/YYYY");
     endDate = moment().format("MM/DD/YYYY");
     $('#auditlogdates').daterangepicker(
-        { 
-            timePicker: false, 
-            timePickerIncrement: 30, 
-            format: 'MM/DD/YYYY',
+    { 
+    	timePicker: false, 
+    	timePickerIncrement: 30, 
+    	format: 'MM/DD/YYYY',
             // startDate: startDate,
             // enddate: endDate
-        },
-        function(start, end, label){
-            startDate = start.format('MM/DD/YYYY');
-            endDate = end.format('MM/DD/YYYY');
-        }
-    );
+          },
+          function(start, end, label){
+          	startDate = start.format('MM/DD/YYYY');
+          	endDate = end.format('MM/DD/YYYY');
+          }
+          );
     //set the dates that will appear in the box
     $('#auditlogdates').data('daterangepicker').setStartDate(startDate);
     $('#auditlogdates').data('daterangepicker').setEndDate(endDate);
@@ -66,20 +66,20 @@ $(function() {
     //listeners
     $( "#buttonGet" ).click(function() {
     	//unload any unchecked boxes prior to timeout hack
-      $("input:checkbox:not(:checked)").each(function (index){
+    	$("input:checkbox:not(:checked)").each(function (index){
           //cleanChart($(this).attr("name"));
           chart.unload({
           	done: function(){
           		//chart unload animation breaks async load, timeout hack workaround
-      				//until API supports multiple actions 1 call to avoid multiple unload() calls
-          		setTimeout(function(){
-		          	$("input:checked").each(function (index){
-		    					getAPIData($(this).val(), $(this).attr("name"));
-		    				});
-		          },230);
-        		}
-      		});
-      });
+							//until API supports multiple actions 1 call to avoid multiple unload() calls
+							setTimeout(function(){
+								$("input:checked").each(function (index){
+									getAPIData($(this).val(), $(this).attr("name"));
+								});
+							},230);
+						}
+					});
+        });
     });
 
     //user dropdown?
@@ -97,15 +97,15 @@ function getAPIData(action, title){
 
     var user = null;
     if (user){
-        url += "/" + user;
+    	url += "/" + user;
     }
     url += "?startdate=" + startDate + "&enddate=" + endDate + "&action=" + action;
 
     $.ajax({
-        url: url
+    	url: url
     }).done(function(data) {
-        if (data.Log.length > 0){
-          var auditData = parseLog(data.Log, title);
+    	if (data.Log.length > 0){
+    		var auditData = parseLog(data.Log, title);
           //TODO: parse data.Users into dropdown issue #20
 
           auditData[0].splice(0,0,'x');
@@ -116,14 +116,14 @@ function getAPIData(action, title){
           // var unchecked = $.map($("input:checkbox:not(:checked)"), function(v){
           // 	return v.name;
           // });
-          chart.load({
+    chart.load({
           	//unload: [unchecked],
-            columns: [
-                auditData[0],
-                auditData[1]
-            ]
+          	columns: [
+          	auditData[0],
+          	auditData[1]
+          	]
           });
-        }
+  }
 
         //testing function, this will fire automatically to test c3 transitions
         // setTimeout(function () {
@@ -136,33 +136,33 @@ function getAPIData(action, title){
         //     });
         // }, 2000);
 
-    }).fail(function(XMLHttpRequest, textStatus, errorThrown) {
-        console.log(textStatus);
-        console.log(errorThrown);
-    });
+}).fail(function(XMLHttpRequest, textStatus, errorThrown) {
+	console.log(textStatus);
+	console.log(errorThrown);
+});
 }
 
 //parse AuditLog data
 var parseLog = function(data, auditAction){
 	var parsedDates = {};
 
-    var dates = [];
-    var counts = [];
+	var dates = [];
+	var counts = [];
 
-    for (var i = 0; i < data.length;i++)
-    {
-        var date = data[i].Date;
+	for (var i = 0; i < data.length;i++)
+	{
+		var date = data[i].Date;
 		//parsedDates[date] = parsedDates[date] ? parsedDates[date] + 1 : 1;
-        parsedDates[date] = parsedDates[date] ? parsedDates[date] + 1 : 1;
-    }
-    counts.push(auditAction);
-    for (var property in parsedDates) {
-        if (parsedDates.hasOwnProperty(property)) {
-            dates.push(property);
-            counts.push(parsedDates[property]);
-        }
-    }
-    return [dates , counts];
+		parsedDates[date] = parsedDates[date] ? parsedDates[date] + 1 : 1;
+	}
+	counts.push(auditAction);
+	for (var property in parsedDates) {
+		if (parsedDates.hasOwnProperty(property)) {
+			dates.push(property);
+			counts.push(parsedDates[property]);
+		}
+	}
+	return [dates , counts];
 };
 
 // //test data
